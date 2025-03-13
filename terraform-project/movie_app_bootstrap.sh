804b078c41967d8436b616bea0bdef73d39b4b39
DLamarG <demondg8@gmail.com>
2025-03-13 12:47:38 -0500

    changed the bash script

--- terraform-project/movie_app_bootstrap.sh
+++ terraform-project/movie_app_bootstrap.sh
@@ -40,6 +40,11 @@ cd /home/ec2-user/Movie_App_CICD/do-assessment3-movie-db/templates/flask-react
 sudo chown -R ec2-user:ec2-user /home/ec2-user/Movie_App_CICD
 
 
+# Get the EC2 instance public IP
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+PUBLIC_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "Public IP: $PUBLIC_IP"
+
 # Update App.jsx in the React app
 cd /home/ec2-user/Movie_App_CICD/do-assessment3-movie-db/templates/flask-react/nginx/reactApp/src
 sed -i "s|const API_URL = 'http://127.0.0.1:5500'|const API_URL = 'http://$PUBLIC_IP:5500'|" App.jsx
